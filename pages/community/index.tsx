98dce8f926323845ebc8e7d97897cc233248cd39
jinyongp <dev.jinyongp@gmail.com>
2022-04-14 22:45:57 +0900

    Update: 내용 추가 및 스타일 변경

--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -1,7 +1,7 @@
 import {
   ChatIcon,
+  CheckCircleIcon,
   PencilIcon,
-  QuestionMarkCircleIcon,
 } from '@heroicons/react/outline';
 import { useRouter } from 'next/router';
 
@@ -9,38 +9,48 @@ export default function Community() {
   const router = useRouter();
 
   return (
-    <ul className="py-8 px-4 flex flex-col gap-4">
-      {[...Array(10)].map((_, i) => (
-        <li
-          key={i}
-          className="p-4 flex flex-col rounded-lg hover-active"
-          onClick={() => router.push('/community/3')}
-        >
-          <span className="text-xs text-gray-800 bg-gray-200 w-fit px-1.5 py-0.5 rounded-full">
-            동네질문
-          </span>
-          <div className="flex gap-1 text-lg">
-            <span className="text-orange-400">Q.</span>
-            <span className="">What is the best mandu restaurant?</span>
-          </div>
-          <span className="text-xs text-gray-500 flex items-center justify-end my-2">
-            니꼬 &bull; 18시간 전
-          </span>
-          <div className="flex gap-4 border-t p-2 border-b-2">
-            <span className="flex gap-1 items-center text-sm text-gray-700">
-              <QuestionMarkCircleIcon className="w-4 h-4" />
-              <span>궁금해요 1</span>
-            </span>
-            <span className="flex gap-1 items-center text-sm text-gray-700">
-              <ChatIcon className="w-4 h-4" />
-              <span>답변 1</span>
-            </span>
-          </div>
-        </li>
-      ))}
+    <div className="py-8">
+      <ul className="flex flex-col gap-2 bg-slate-100">
+        {[...Array(10)].map((_, i) => (
+          <li
+            key={i}
+            className="flex flex-col bg-white cursor-pointer"
+            onClick={() => router.push('/community/3')}
+          >
+            <div className="flex flex-col gap-2 p-4">
+              <span className="text-xs text-gray-700 bg-gray-100 w-fit px-1.5 py-0.5 rounded-sm">
+                동네질문
+              </span>
+              <div className="flex gap-1 font-normal text-gray-800">
+                <span className="text-orange-400">Q.</span>
+                <span className="">What is the best mandu restaurant?</span>
+              </div>
+              <p className="text-sm text-gray-600 text-ellipsis line-clamp-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia
+                distinctio nulla tempora quas! Magnam cum eos dolore consectetur
+                hic aut error eveniet dolorem quas recusandae, incidunt
+                nesciunt, iste libero harum.
+              </p>
+              <span className="text-xs text-gray-500 flex items-center justify-end">
+                니꼬 &bull; 18시간 전
+              </span>
+            </div>
+            <div className="flex gap-4 border-t text-sm text-gray-500 px-4 py-2">
+              <span className="flex gap-1 items-center">
+                <CheckCircleIcon className="w-4 h-4" />
+                <span>궁금해요 1</span>
+              </span>
+              <span className="flex gap-1 items-center">
+                <ChatIcon className="w-4 h-4" />
+                <span>답변 1</span>
+              </span>
+            </div>
+          </li>
+        ))}
+      </ul>
       <button className="fixed bottom-12 right-12 bg-orange-400 hover:bg-orange-500 transition-colors text-white p-4 rounded-full pointer-cursor shadow-md">
         <PencilIcon className="w-6 h-6" />
       </button>
-    </ul>
+    </div>
   );
 }
