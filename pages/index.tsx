ac170d3861adc6770c4230f5dfb67e90b2b4bba2
jinyongp <dev.jinyongp@gmail.com>
2022-04-14 22:22:43 +0900

    Style: 스타일 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,13 +17,15 @@ export default function Home() {
               <div className="w-24 h-24 bg-slate-400 rounded-lg" />
               <div className="ml-4 flex flex-col">
                 <h3 className="text-gray-700 text-sm">New iPhone 14</h3>
-                <span className="text-gray-500 text-xs">Black</span>
+                <span className="text-gray-500 text-xs">
+                  Black &bull; 10분 전
+                </span>
                 <span className="text-gray-900 font-medium text-lg mt-2">
                   $95
                 </span>
               </div>
             </div>
-            <div className="flex items-end gap-4 text-gray-700">
+            <div className="flex items-end gap-2 text-gray-700 text-xs">
               <div className="flex items-center gap-0.5 text-pink-400">
                 <HeartIcon className="w-4 h-4" />
                 <span>100</span>
--- pages/items/[id].tsx
+++ pages/items/[id].tsx
@@ -4,24 +4,24 @@ export default function Item() {
   return (
     <div className="py-8 px-4">
       <div className="aspect-square bg-slate-300 rounded-2xl" />
-      <div className="flex gap-2 py-4 items-center cursor-pointer border-b">
+      <div className="flex gap-2 py-4 items-center border-b cursor-pointer">
         <div className="w-12 aspect-square rounded-full bg-slate-300" />
-        <div className="flex flex-col group">
+        <div className="flex flex-col grow">
           <p className="text-gray-700 font-medium text-sm">Steve Jebs</p>
-          <p className="text-gray-500 font-medium text-xs group-hover:translate-x-1 transition-transform">
+          <p className="text-gray-500 font-medium text-xs">
             View profile &rarr;
           </p>
         </div>
       </div>
       <div className="flex flex-col my-4">
-        <div className="flex justify-between items-center">
-          <h1 className="text-3xl font-semibold text-gray-800 mt-2 mb-1">
-            Galaxy S50
-          </h1>
-          <p className="text-2xl font-medium bg-orange-500 shadow w-fit text-white px-2 rounded-md">
-            $140
-          </p>
-        </div>
+        <h1
+          className="text-3xl font-semibold text-gray-800 mt-2 mb-1 cursor-pointer"
+          onClick={({ target }) =>
+            (target as HTMLElement).scrollIntoView({ behavior: 'smooth' })
+          }
+        >
+          Galaxy S50
+        </h1>
         <p className="my-4 text-sm text-gray-700">
           My money&apos;s in that office, right? If she start giving me some
           bullshit about it ain&apos;t there, and we got to go someplace else
@@ -32,18 +32,21 @@ export default function Item() {
           ni**a Winston or anybody else is in there, you the first motherfucker
           to get shot. You understand?
         </p>
-        <div className="flex gap-2">
-          <button className="btn-primary">Talk to seller</button>
-          <button className="btn-default w-fit border-none hover:bg-pink-100 hover:text-pink-400">
-            <HeartIcon className="w-6 h-6" />
-          </button>
+      </div>
+      <div className="p-3 flex items-center justify-between gap-2 divide-x fixed z-50 bottom-0 left-0 w-full bg-white border-t">
+        <button className="border-none text-gray-400 hover:text-pink-400 shadow-none">
+          <HeartIcon className="w-6 h-6" />
+        </button>
+        <div className="px-2 grow">
+          <span className="text-gray-700 font-medium">$190</span>
         </div>
+        <button className="btn-primary w-fit px-4">Chat</button>
       </div>
       <div className="py-2">
-        <h2 className="text-gray-700 font-semibold text-2xl my-2">
+        <h2 className="text-gray-700 font-semibold text-2xl border-t py-4">
           Similar items
         </h2>
-        <div className="grid grid-cols-2 gap-2">
+        <div className="grid grid-cols-2 gap-2 mb-8">
           {[...Array(10)].map((_, i) => (
             <div
               key={i}
